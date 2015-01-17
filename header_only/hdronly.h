82618ee506602ca04b62a43403aec7fe08b6d600
Aaditya Kalsi <akalsi.dev@gmail.com>
2015-01-17 11:24:45 -0500

    test-empty-proj

---
+++ header_only/hdronly.h
@@ -0,0 +1,9 @@
+#ifndef HDRONLY_H
+#define HDRONLY_H
+
+inline
+int hdronly_test(void) {
+    return 1;
+}
+
+#endif//HDRONLY_H
---
+++ test_exec/test_exe.c
@@ -0,0 +1,7 @@
+
+#include <stdio.h>
+
+int main(int argc, const char** argv) {
+    printf("Test exe done!\n");
+    return 0;
+}
